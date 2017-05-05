81bbf532395ae53497e54c8bbb7ff95b6e68a15a
Vincent van Haaff <vincent@vanhaaff.com>
2017-05-05 16:38:25 -0700

    more changes

--- ios/CDVBackgroundGeolocation/Location.m
+++ ios/CDVBackgroundGeolocation/Location.m
@@ -99,6 +99,9 @@ enum {
 {
     NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:11];
     
+    UIDevice *device = [UIDevice currentDevice];
+    NSString *uniqueId = [[device identifierForVendor] UUIDString];
+    
     if (time != nil) [dict setObject:[NSNumber numberWithDouble:([time timeIntervalSince1970] * 1000)] forKey:@"time"];
     if (accuracy != nil) [dict setObject:accuracy forKey:@"accuracy"];
     if (altitudeAccuracy != nil) [dict setObject:altitudeAccuracy forKey:@"altitudeAccuracy"];
